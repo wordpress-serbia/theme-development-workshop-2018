1fca6f3c543df40556cc7844f6b1a3fb0b36a374
Milana Cap <milanazzap@gmail.com>
2018-09-23 01:15:40 +0200

    R-02 - Template parts and conditionals. (#5)

--- wp-content/themes/radionica/page.php
+++ wp-content/themes/radionica/page.php
@@ -14,8 +14,7 @@ get_header();
 	if ( have_posts() ) :
 		while ( have_posts() ) : the_post();
 
-			the_title( '<h1 class="entry-title">', '</h1>' );
-			the_content();
+			get_template_part( '/template-parts/entry' );
 
 		endwhile; // have_posts()
 	endif; // have_posts()
