5e5665761ad5f904d317df663adcb6df801983ee
Milana Cap <milanazzap@gmail.com>
2018-09-22 19:25:19 +0200

    R-02 - Bare minimum for theme. (#1) (#2)
    * R-02 - Bare minimum for theme: style.css, index.php, header.php, footer.php

---
+++ wp-content/themes/radionica/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Main Template File
+ *
+ * @link https://developer.wordpress.org/themes/template-files-section/post-template-files/
+ * @link https://wphierarchy.com/
+ *
+ * @package WordPress
+ */
+get_header();
+
+
+get_footer();
