4b7314e6a9c1b647cd47c8cd76af4c124f082fb1
Milana <milanazzap@gmail.com>
2018-10-07 00:31:43 +0200

    R-03 - Theme Support - Post Thumbnails.

--- wp-content/themes/radionica/functions.php
+++ wp-content/themes/radionica/functions.php
@@ -11,10 +11,15 @@
  * Note that this function is hooked into the 'after_setup_theme' hook, which
  * runs before the 'init' hook. The 'init' hook is too late for some features, such
  * as indicating support for post thumbnails.
+ *
+ * @link https://developer.wordpress.org/reference/functions/add_theme_support/
  */
 function radionica_setup() {
 	/**
 	 * Custom Logo
+	 *
+	 * @link https://developer.wordpress.org/reference/functions/add_theme_support/#custom-logo
+	 * @link https://developer.wordpress.org/themes/functionality/custom-logo/
 	 */
 	add_theme_support( 'custom-logo', array(
 		'height'      => 100,
@@ -25,16 +30,22 @@ function radionica_setup() {
 
 	/**
 	 * Automatic Feed Links
+	 *
+	 * @link https://developer.wordpress.org/reference/functions/add_theme_support/#feed-links
 	 */
 	add_theme_support( 'automatic-feed-links' );
 
 	/**
 	 * Title Tag
+	 *
+	 * @link https://developer.wordpress.org/reference/functions/add_theme_support/#title-tag
 	 */
 	add_theme_support( 'title-tag' );
 
 	/**
 	 * Custom Background
+	 *
+	 * @link https://developer.wordpress.org/reference/functions/add_theme_support/#custom-background
 	 */
 	add_theme_support( 'custom-background', array(
 		'default-image'      => get_template_directory_uri() . '/images/WordPress-logotype-wmark.png',
@@ -46,6 +57,21 @@ function radionica_setup() {
 		'default-attachment' => 'scroll', // 'scroll', 'fixed'
 		'default-color'      => '#ff0000',
 	) );
+
+	/**
+	 * Post Thumbnails
+	 *
+	 * @link https://developer.wordpress.org/reference/functions/add_theme_support/#post-thumbnails
+	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+	 */
+	add_theme_support( 'post-thumbnails' );
+	/**
+	 * Add cusrom image sizes
+	 *
+	 * @link https://developer.wordpress.org/reference/functions/add_image_size/
+	 */
+	add_image_size( 'featured-single', 1200, 400, true );
+	add_image_size( 'featured-archive', 400, 200, array( 'center', 'center' ) );
 }
 add_action( 'after_setup_theme', 'radionica_setup' );
 
