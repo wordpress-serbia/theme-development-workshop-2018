cc46663b91579fb0b70fd0cbf7e339403b0cb8e0
Milana Cap <milanazzap@gmail.com>
2018-10-07 01:54:03 +0200

    R-03 (#17)
    * R-03 - Custom Logo (#9)
    
    *  R-03 - Theme support - Automatic Feed Links and Title Tag. (#10)
    
    *  R-03 - Theme support - Custom Background (#11)
    
    *  R-03 - Basic styling (#12)
    
    *  R-03 - Theme Support - Post Thumbnails. (#13)
    
    *  R-03 - Theme Support - Custom Header. (#14)
    
    *  R-03 - Theme Support - Navigation Menus. (#15)
    
    *  R-03 - Theme Support - Documentation and custom logo filter example. (#16)

--- wp-content/themes/radionica/functions.php
+++ wp-content/themes/radionica/functions.php
@@ -6,6 +6,97 @@
  */
 
 /**
+ * Sets up theme defaults and registers support for various WordPress features.
+ *
+ * Note that this function is hooked into the 'after_setup_theme' hook, which
+ * runs before the 'init' hook. The 'init' hook is too late for some features, such
+ * as indicating support for post thumbnails.
+ *
+ * @link https://developer.wordpress.org/reference/functions/add_theme_support/
+ */
+function radionica_setup() {
+	/**
+	 * Custom Logo
+	 *
+	 * @link https://developer.wordpress.org/reference/functions/add_theme_support/#custom-logo
+	 * @link https://developer.wordpress.org/themes/functionality/custom-logo/
+	 */
+	add_theme_support( 'custom-logo', array(
+		'height'      => 100,
+		'width'       => 400,
+		'flex-width'  => true,
+		'flex-height' => true
+	) );
+
+	/**
+	 * Automatic Feed Links
+	 *
+	 * @link https://developer.wordpress.org/reference/functions/add_theme_support/#feed-links
+	 */
+	add_theme_support( 'automatic-feed-links' );
+
+	/**
+	 * Title Tag
+	 *
+	 * @link https://developer.wordpress.org/reference/functions/add_theme_support/#title-tag
+	 */
+	add_theme_support( 'title-tag' );
+
+	/**
+	 * Custom Background
+	 *
+	 * @link https://developer.wordpress.org/reference/functions/add_theme_support/#custom-background
+	 */
+	add_theme_support( 'custom-background', array(
+		'default-image'      => get_template_directory_uri() . '/images/WordPress-logotype-wmark.png',
+		'default-preset'     => 'custom', // 'default', 'fill', 'fit', 'repeat', 'custom'
+		'default-position-x' => 'center', // 'left', 'center', 'right'
+		'default-position-y' => 'top',    // 'top', 'center', 'bottom'
+		'default-size'       => 'auto',   // 'auto', 'contain', 'cover'
+		'default-repeat'     => 'repeat', // 'repeat-x', 'repeat-y', 'repeat', 'no-repeat'
+		'default-attachment' => 'scroll', // 'scroll', 'fixed'
+		'default-color'      => '#ff0000',
+	) );
+
+	/**
+	 * Post Thumbnails
+	 *
+	 * @link https://developer.wordpress.org/reference/functions/add_theme_support/#post-thumbnails
+	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+	 */
+	add_theme_support( 'post-thumbnails' );
+	/**
+	 * Add cusrom image sizes
+	 *
+	 * @link https://developer.wordpress.org/reference/functions/add_image_size/
+	 */
+	add_image_size( 'featured-single', 1200, 400, true );
+	add_image_size( 'featured-archive', 400, 200, array( 'center', 'center' ) );
+
+	/**
+	 * Custom Header
+	 *
+	 * @link https://developer.wordpress.org/reference/functions/add_theme_support/#custom-header
+	 * @link https://developer.wordpress.org/themes/functionality/custom-headers/
+	 */
+	add_theme_support( 'custom-header', array(
+		'width'  => 1200,
+		'height' => 400
+	) );
+
+	/**
+	 * Register Navigation Menus
+	 *
+	 * @link https://developer.wordpress.org/reference/functions/register_nav_menus/
+	 * @link https://developer.wordpress.org/themes/functionality/navigation-menus/
+	 */
+	register_nav_menus( array(
+		'header' => esc_html__( 'Header Menu', 'radionica' )
+	) );
+}
+add_action( 'after_setup_theme', 'radionica_setup' );
+
+/**
  * Load scripts and styles.
  *
  * This function is attached to
@@ -16,3 +107,86 @@ function radionica_enqueue_scripts() {
 	wp_enqueue_style( 'radionica-style', get_stylesheet_uri() );
 }
 add_action( 'wp_enqueue_scripts', 'radionica_enqueue_scripts' );
+
+/**
+ * Filters the custom logo output.
+ *
+ * @param string $html Custom logo HTML output.
+ * @return string      Returns filtered custom logo HTML output.
+ */
+function radionica_get_custom_logo( $html ) {
+	$custom_logo_id = get_theme_mod( 'custom_logo' );
+	/**
+	 * This one was added just to avoid
+	 * 'undefined' notice. For real project
+	 * please check all the conditionals in
+	 * function.
+	 *
+	 * @link https://developer.wordpress.org/reference/functions/get_custom_logo/
+	 */
+	$custom_logo_attr = array(
+		'class'    => 'custom-logo',
+		'itemprop' => 'logo',
+	);
+	/*
+	 * If the alt attribute is not empty, there's no need to explicitly pass
+	 * it because wp_get_attachment_image() already adds the alt attribute.
+	 */
+	$html = sprintf( '<a href="%1$s" class="custom-logo-link radionica" rel="home" itemprop="url">%2$s</a>',
+		esc_url( home_url( '/' ) ),
+		wp_get_attachment_image( $custom_logo_id, 'full', false, $custom_logo_attr )
+	);
+
+	return $html;
+}
+add_filter( 'get_custom_logo', 'radionica_get_custom_logo' );
+
+/**
+ * Modify document title
+ *
+ * Change the document title appearance. This function is
+ * attached to 'document_title_parts' filter hook.
+ *
+ * @param array $title {
+ *     The document title parts. In that order.
+ *
+ *     @type string $title   Title of the viewed page.
+ *     @type string $page    Optional. Page number if paginated.
+ *     @type string $tagline Optional. Site description when on home page.
+ *     @type string $site    Optional. Site title when not on home page.
+ * }
+ * @return array        Returns filtered document title parts
+ */
+function radionica_document_title_parts( $title ) {
+	if ( is_front_page() ) :
+		$title['tagline'] = $title['title'];
+		$title['title']   = esc_html__( 'This is the home', 'radionica' );
+	elseif ( is_single() ) :
+		$title['title'] = '#' . get_the_ID() . ' ' . get_the_title( get_the_ID() );
+		$title['site']  = get_bloginfo( 'description', 'display' ) . ' / ' . $title['site'];
+	endif;
+
+	return $title;
+}
+add_filter( 'document_title_parts', 'radionica_document_title_parts' );
+
+/**
+ * Document title separator
+ *
+ * Change the separator which will appear between
+ * each document title part. This function is attached
+ * to 'document_title_separator' filter hook.
+ *
+ * @param  string $sep Separator for document title, default '-'
+ * @return string      Returns filtered separator
+ */
+function radionica_document_title_separator( $sep ) {
+	if ( is_front_page() ) :
+		$sep = '//';
+	elseif ( is_single() ) :
+		$sep = '/';
+	endif;
+
+	return $sep;
+}
+add_filter( 'document_title_separator', 'radionica_document_title_separator' );
--- wp-content/themes/radionica/header.php
+++ wp-content/themes/radionica/header.php
@@ -20,19 +20,69 @@
 
 	<header>
 		<div class="wrapper">
-			<?php if ( is_front_page() ) : ?>
-				<h1 class="site-title">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-				</h1>
-			<?php else : ?>
-				<p class="site-title">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-				</p>
-			<?php endif; // is_front_page() ?>
+			<?php
+				if ( has_custom_logo() ) :
+					the_custom_logo();
+				else :
+					if ( is_front_page() ) : ?>
+						<h1 class="site-title">
+							<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+						</h1>
+					<?php else : ?>
+						<p class="site-title">
+							<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+						</p>
+					<?php endif; // is_front_page() ?>
 
-			<p class="site-description">
-				<?php bloginfo( 'description' ); ?>
-			</p>
+					<p class="site-description">
+						<?php bloginfo( 'description' ); ?>
+					</p>
+				<?php endif; // has_custom_logo()
+
+				/**
+				 * Menu
+				 *
+				 * @link https://developer.wordpress.org/reference/functions/wp_nav_menu/
+				 */
+				wp_nav_menu( array(
+					'theme_location' => 'header',
+					'menu_class'     => 'radionica-menu',
+					'container'      => 'nav',
+					'after'          => '<span>icon</span>',
+					'link_before'    => '&rarr; ',
+					'depth'          => 1,
+					'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+					'item_spacing'   => 'discard'
+				) );
+
+				/**
+				 * Header image
+				 *
+				 * This is code we used on workshop
+				 * but this is easily achieved with
+				 * the_custom_header_markup()
+				 */
+				if ( get_header_image() ) :
+					// Get custom header object.
+					$custom_header = get_custom_header();
+					// Get uploaded image ID.
+					$header_id = $custom_header->attachment_id; ?>
+
+					<div class="wp-custom-header">
+						<img src="<?php header_image(); ?>" alt="<?php echo esc_attr( get_the_title( $header_id ) ); ?>" >
+					</div>
+
+				<?php endif; // get_header_image()
+
+				/**
+				 * Much easier and faster with the_custom_header_markup().
+				 * @link https://developer.wordpress.org/reference/functions/the_custom_header_markup/
+				 *
+				 * Also, it uses srcset for responsive images.
+				 * @link https://make.wordpress.org/core/2015/11/10/responsive-images-in-wordpress-4-4/
+				 */
+				// the_custom_header_markup();
+			?>
 		</div><!-- wrapper -->
 	</header>
 
