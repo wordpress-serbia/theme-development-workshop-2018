4ce1614d1f6c1779dee2f667bd4841e5f8430c39
Milana Cap <milanazzap@gmail.com>
2018-10-17 23:56:16 +0200

    R-04 - Walker_Nav_Menu (#17)
    * R-04 - Walker_Nav_Menu::start_lvl().
    
    * R-04 - Walker_Nav_Menu::start_el().
    
    * R-04 - Walker_Nav_Menu docs.
    
    * R-04 - Walker_Nav_Menu - Custom menu markup.
    
    * R-04 - Walker_Nav_Menu - setup.
    
    * R-04 - Custom menu - Post title and menu item description.
    
    * R-04 - Custom menu - Post excerpt.
    
    * R-04 - Custom menu - Post thumbnail.
    
    * R-04 - Custom menu - Escaping and security checks.
    
    * R-04 - Custom menu - Docs.

--- README.md
+++ README.md
@@ -28,6 +28,12 @@ Sadržaj:
   - [Post Thumbnails](#post-thumbnails)
   - [Custom Header](#custom-header)
   - [Navigation Menus](#navigation-menus)
+- [R-04 - Walker_Nav_Menu](#r-04)
+  - [Walker_Nav_Menu::start_lvl](#walker_nav_menustart_lvl)
+  - [Walker_Nav_Menu::end_lvl](#walker_nav_menuend_lvl)
+  - [Walker_Nav_Menu::start_el](#walker_nav_menustart_el)
+  - [Walker_Nav_Menu::end_el](#walker_nav_menuend_el)
+  - [Markup prilagođenog izbornika](#markup-prilagođenog-izbornika)
 
 # R-01
 
@@ -167,3 +173,80 @@ Smernice za pisanje kvalitetnog CSS koda koji je moguće održavati na duge staz
 - [Navigation Menus](https://developer.wordpress.org/themes/functionality/navigation-menus/)
 - [register_nav_menus()](https://developer.wordpress.org/reference/functions/register_nav_menus/)
 - [wp_nav_menu()](https://developer.wordpress.org/reference/functions/wp_nav_menu/)
+
+# R-04
+
+- [Walker_Nav_Menu](https://developer.wordpress.org/reference/classes/walker_nav_menu/)
+
+## Walker_Nav_Menu::start_lvl
+
+- odnosi se na submenu, vraća `<ul>`
+- [Walker_Nav_Menu::start_lvl()](https://developer.wordpress.org/reference/classes/walker_nav_menu/start_lvl/)
+- [Objašnjenje za $item_spacing](https://make.wordpress.org/core/2016/11/07/whitespace-changes-in-navigation-for-4-7/)
+
+## Walker_Nav_Menu::end_lvl
+
+- odnosi se na submenu, vraća `</ul>`
+- [Walker_Nav_Menu::end_lvl()](https://developer.wordpress.org/reference/classes/walker_nav_menu/end_lvl/)
+
+## Walker_Nav_Menu::start_el
+
+- odnosi se na svaku stavku izbornika, vraća `<li>` i `<a></a>`
+- [Walker_Nav_Menu::start_el()](https://developer.wordpress.org/reference/classes/walker_nav_menu/start_el/)
+
+## Walker_Nav_Menu::end_el
+
+- odnosi se na svaku stavku izbornika, vraća `</li>`
+- [Walker_Nav_Menu::end_el()](https://developer.wordpress.org/reference/classes/walker_nav_menu/end_el/)
+
+## Markup prilagođenog izbornika
+
+```
+<nav class="custom-navigation">
+  <ul>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Category</a>
+      <ul>
+        <li>
+          <div class="entry-title">
+            <a href="#">Post title</a>
+            <span>This is description</span>
+          </div>
+          <div class="entry-summary">
+            <p>The Accessibility Team works to make WordPress accessible to as many people as possible. This means making sure people are not just able to read web pages but also to maintain websites. You are a part of this mission. You benefit from this mission. So in the spirit of one of the largest open-source communities in the world, let’s work on universal accessibility.</p>
+          </div>
+          <div class="entry-image">
+            <img src="https://images.unsplash.com/photo-1495774539583-885e02cca8c2?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=b4876c41d5e7585486007cab84b34512">
+          </div>
+        </li>
+        <li>
+          <div class="entry-title">
+            <a href="#">Post title</a>
+            <span>This is description</span>
+          </div>
+          <div class="entry-summary">
+            <p>In this Make WordPress Accessibility Handbook you will learn what the best practices are for web accessibility, the many great accessibility tools, the testing we do to improve WordPress, themes, and plugins, and how to get involved in WordPress accessibility.</p>
+          </div>
+          <div class="entry-image">
+            <img src="https://s.w.org/style/images/about/WordPress-logotype-alternative.png">
+          </div>
+        </li>
+        <li>
+          <div class="entry-title">
+            <a href="#">Post title</a>
+            <span>This is description</span>
+          </div>
+          <div class="entry-summary">
+            <p>Below are the web essentials you’ll need to make your site accessible. Other handbook pages explain why these standards are critical to your site. This page tells you how to quickly implement the standards, with WordPress-specific code examples, guidelines, and best-practices.</p>
+          </div>
+          <div class="entry-image">
+            <img src="https://s.w.org/style/images/about/WordPress-logotype-wmark.png">
+          </div>
+        </li>
+      </ul>
+    </li>
+  </ul>
+</nav>
+```
+
+- [apply_filters( 'the_excerpt', $excerpt )](https://developer.wordpress.org/reference/functions/the_excerpt/)
---
+++ wp-content/themes/radionica/inc/class-custom-walker-nav-menu.php
@@ -0,0 +1,189 @@
+<?php
+/**
+ * Custom Walker Nav Menu
+ *
+ * @package WordPress
+ */
+if ( ! class_exists( 'Custom_Walker_Nav_Menu' ) ) :
+	/**
+	 * Custom_Walker_Nav_Menu
+	 *
+	 * @extends Walker_Nav_Menu
+	 */
+	class Custom_Walker_Nav_Menu extends Walker_Nav_Menu {
+
+		/**
+		 * Starts the element output.
+		 *
+		 * @since 3.0.0
+		 * @since 4.4.0 The {@see 'nav_menu_item_args'} filter was added.
+		 *
+		 * @see Walker::start_el()
+		 *
+		 * @param string   $output Used to append additional content (passed by reference).
+		 * @param WP_Post  $item   Menu item data object.
+		 * @param int      $depth  Depth of menu item. Used for padding.
+		 * @param stdClass $args   An object of wp_nav_menu() arguments.
+		 * @param int      $id     Current item ID.
+		 */
+		public function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+			if ( isset( $args->item_spacing ) && 'discard' === $args->item_spacing ) {
+				$t = '';
+				$n = '';
+			} else {
+				$t = "\t";
+				$n = "\n";
+			}
+			$indent = ( $depth ) ? str_repeat( $t, $depth ) : '';
+
+			$classes = empty( $item->classes ) ? array() : (array) $item->classes;
+			$classes[] = 'menu-item-' . $item->ID;
+
+			/**
+			 * Filters the arguments for a single nav menu item.
+			 *
+			 * @since 4.4.0
+			 *
+			 * @param stdClass $args  An object of wp_nav_menu() arguments.
+			 * @param WP_Post  $item  Menu item data object.
+			 * @param int      $depth Depth of menu item. Used for padding.
+			 */
+			$args = apply_filters( 'nav_menu_item_args', $args, $item, $depth );
+
+			/**
+			 * Filters the CSS class(es) applied to a menu item's list item element.
+			 *
+			 * @since 3.0.0
+			 * @since 4.1.0 The `$depth` parameter was added.
+			 *
+			 * @param array    $classes The CSS classes that are applied to the menu item's `<li>` element.
+			 * @param WP_Post  $item    The current menu item.
+			 * @param stdClass $args    An object of wp_nav_menu() arguments.
+			 * @param int      $depth   Depth of menu item. Used for padding.
+			 */
+			$class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args, $depth ) );
+			$class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
+
+			/**
+			 * Filters the ID applied to a menu item's list item element.
+			 *
+			 * @since 3.0.1
+			 * @since 4.1.0 The `$depth` parameter was added.
+			 *
+			 * @param string   $menu_id The ID that is applied to the menu item's `<li>` element.
+			 * @param WP_Post  $item    The current menu item.
+			 * @param stdClass $args    An object of wp_nav_menu() arguments.
+			 * @param int      $depth   Depth of menu item. Used for padding.
+			 */
+			$id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args, $depth );
+			$id = $id ? ' id="' . esc_attr( $id ) . '"' : '';
+
+			$output .= $indent . '<li' . $id . $class_names .'>';
+
+			$atts = array();
+			$atts['title']  = ! empty( $item->attr_title ) ? $item->attr_title : '';
+			$atts['target'] = ! empty( $item->target )     ? $item->target     : '';
+			$atts['rel']    = ! empty( $item->xfn )        ? $item->xfn        : '';
+			$atts['href']   = ! empty( $item->url )        ? $item->url        : '';
+
+			/**
+			 * Filters the HTML attributes applied to a menu item's anchor element.
+			 *
+			 * @since 3.6.0
+			 * @since 4.1.0 The `$depth` parameter was added.
+			 *
+			 * @param array $atts {
+			 *     The HTML attributes applied to the menu item's `<a>` element, empty strings are ignored.
+			 *
+			 *     @type string $title  Title attribute.
+			 *     @type string $target Target attribute.
+			 *     @type string $rel    The rel attribute.
+			 *     @type string $href   The href attribute.
+			 * }
+			 * @param WP_Post  $item  The current menu item.
+			 * @param stdClass $args  An object of wp_nav_menu() arguments.
+			 * @param int      $depth Depth of menu item. Used for padding.
+			 */
+			$atts = apply_filters( 'nav_menu_link_attributes', $atts, $item, $args, $depth );
+
+			$attributes = '';
+			foreach ( $atts as $attr => $value ) {
+				if ( ! empty( $value ) ) {
+					$value = ( 'href' === $attr ) ? esc_url( $value ) : esc_attr( $value );
+					$attributes .= ' ' . $attr . '="' . $value . '"';
+				}
+			}
+
+			/** This filter is documented in wp-includes/post-template.php */
+			$title = apply_filters( 'the_title', $item->title, $item->ID );
+
+			/**
+			 * Filters a menu item's title.
+			 *
+			 * @since 4.4.0
+			 *
+			 * @param string   $title The menu item's title.
+			 * @param WP_Post  $item  The current menu item.
+			 * @param stdClass $args  An object of wp_nav_menu() arguments.
+			 * @param int      $depth Depth of menu item. Used for padding.
+			 */
+			$title = apply_filters( 'nav_menu_item_title', $title, $item, $args, $depth );
+
+			$item_output = $args->before;
+
+			// If posts are found in first level submenu.
+			if ( $depth == 1 && $item->object == 'post' ) :
+				$post_id     = $item->object_id;
+				$description = $item->description;
+				$excerpt     = get_the_excerpt( $post_id );
+
+				// Entry title.
+				$item_output .= '<div class="entry-title">';
+				$item_output .= '<a'. $attributes .'>';
+				$item_output .= $args->link_before . $title . $args->link_after;
+				$item_output .= '</a>';
+				if ( $description ) :
+					$item_output .= '<span>' . esc_html( $description ) . '</span>';
+				endif; // $description
+				$item_output .= '</div>';
+
+				// Entry summary.
+				if ( $excerpt ) :
+					$item_output .= '<div class="entry-summary">';
+					$item_output .= apply_filters( 'the_excerpt', $excerpt );
+					$item_output .= '</div>';
+				endif; // $excerpt
+
+				// Entry image.
+				if ( has_post_thumbnail() ) :
+					$item_output .= '<div class="entry-image">';
+					$item_output .= get_the_post_thumbnail( $post_id, 'medium' );
+					$item_output .= '</div>';
+				endif; // has_post_thumbnail()
+			else :
+				$item_output .= '<a'. $attributes .'>';
+				$item_output .= $args->link_before . $title . $args->link_after;
+				$item_output .= '</a>';
+			endif;
+
+			$item_output .= $args->after;
+
+			/**
+			 * Filters a menu item's starting output.
+			 *
+			 * The menu item's starting output only includes `$args->before`, the opening `<a>`,
+			 * the menu item's title, the closing `</a>`, and `$args->after`. Currently, there is
+			 * no filter for modifying the opening and closing `<li>` for a menu item.
+			 *
+			 * @since 3.0.0
+			 *
+			 * @param string   $item_output The menu item's starting HTML output.
+			 * @param WP_Post  $item        Menu item data object.
+			 * @param int      $depth       Depth of menu item. Used for padding.
+			 * @param stdClass $args        An object of wp_nav_menu() arguments.
+			 */
+			$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+		}
+
+	}
+endif; // ! class_exists( 'Custom_Walker_Nav_Menu' )
