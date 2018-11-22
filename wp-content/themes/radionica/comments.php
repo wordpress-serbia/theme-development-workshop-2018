1d6db6d914514c115e32f3277d80caa1713ecab9
Milana <milanazzap@gmail.com>
2018-11-22 23:50:16 +0100

    R-05 - Other comments template  tags.

--- wp-content/themes/radionica/comments.php
+++ wp-content/themes/radionica/comments.php
@@ -18,21 +18,72 @@
 if ( post_password_required() ) {
 	return;
 }
+?>
 
-if ( have_comments() ) : ?>
+<div id="comments" class="comments">
+	<?php if ( have_comments() ) : ?>
 
-	<ol class="commentlist">
-		<?php wp_list_comments( array(
-			'avatar_size'       => false,
-			'reverse_top_level' => true,
-			'callback'          => 'radionica_html5_comment'
-		) ); ?>
-	</ol>
+		<h2 class="comments-title">
+			<?php
+			$comments_number = get_comments_number();
+			if ( '1' === $comments_number ) {
+				/* translators: %s: post title */
+				printf( _x( 'One Reply to &ldquo;%s&rdquo;', 'comments title', 'radionica' ), get_the_title() );
+			} else {
+				printf(
+					/* translators: 1: number of comments, 2: post title */
+					_nx(
+						'%1$s Reply to &ldquo;%2$s&rdquo;',
+						'%1$s Replies to &ldquo;%2$s&rdquo;',
+						$comments_number,
+						'comments title',
+						'radionica'
+					),
+					number_format_i18n( $comments_number ),
+					get_the_title()
+				);
+			}
+			?>
+		</h2>
 
-<?php endif; // have_comments()
+		<?php the_comments_navigation(); ?>
 
-comment_form();
+		<ol class="commentlist">
+			<?php wp_list_comments( array(
+				'avatar_size'       => false,
+				'reverse_top_level' => true,
+				'callback'          => 'radionica_html5_comment'
+			) ); ?>
+		</ol>
 
+		<?php
+
+			the_comments_navigation( array(
+				'prev_text' => esc_html__( '&larr; Previous', 'radionica' ),
+				'next_text' => esc_html__( 'Next &rarr;', 'radionica' )
+			));
+
+	endif; // have_comments()
+
+	if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
+		esc_html_e( 'Comments are closed.', 'radionica' );
+	endif;
+
+	if ( comments_open() ) :
+		// Dashbord -> Settings -> Discussion -> Other comment settings
+		if ( get_option( 'comment_registration' ) && ! is_user_logged_in() ) :
+			/* Translators: 1: Log in URL */
+			printf( __( 'You must be <a href="%s">logged in</a> to post a comment.', 'starter' ),
+				esc_url( wp_login_url( get_permalink() ) ) // WPCS: XSS ok.
+			);
+		else :
+			comment_form();
+		endif;
+	endif;
+	?>
+</div>
+
+<?php
 /**
  * Comment form arguments
  */
