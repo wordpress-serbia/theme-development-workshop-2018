1995beef6d0dbcc4d904e6985b425276894ad2c7
Milana Cap <milanazzap@gmail.com>
2018-11-27 15:58:22 +0100

    Workshop 5.

---
+++ wp-content/themes/radionica/comments.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Comments
+ *
+ * Template for displaying comments list and form on single posts.
+ *
+ * @link https://developer.wordpress.org/themes/template-files-section/partial-and-miscellaneous-template-files/
+ *
+ * @package WordPress
+ */
+
+/*
+ * If the current post is protected by a password and
+ * the visitor has not yet entered the password we will
+ * return early without loading the comments.
+ */
+if ( post_password_required() ) {
+	return;
+}
+
+if ( have_comments() ) : ?>
+
+	<ol class="commentlist">
+		<?php wp_list_comments( array(
+			'avatar_size'       => false,
+			'reverse_top_level' => true,
+			'callback'          => 'radionica_html5_comment'
+		) ); ?>
+	</ol>
+
+<?php endif; // have_comments()
+
+comment_form();
