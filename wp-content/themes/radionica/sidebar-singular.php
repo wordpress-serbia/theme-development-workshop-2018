eebeff956751c9f189e05d4269d2840125d2f618
Milana <milanazzap@gmail.com>
2018-12-19 22:46:25 +0100

    R-07 - Widget areas outside of sidebar.

--- wp-content/themes/radionica/sidebar-singular.php
+++ wp-content/themes/radionica/sidebar-singular.php
@@ -6,6 +6,6 @@
  */
 if ( is_active_sidebar( 'sidebar-singular' ) ) : ?>
 	<aside class="widget-area widget-area-sidebar" role="complementary">
-	    <?php dynamic_sidebar( 'sidebar-singular' ); ?>
+		<?php dynamic_sidebar( 'sidebar-singular' ); ?>
 	</aside>
 <?php endif;
