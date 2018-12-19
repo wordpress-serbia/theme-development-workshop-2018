3cd144ba11678ad0625e8a1e9b3ee5fb77cbfbd0
Milana <milanazzap@gmail.com>
2018-12-19 20:04:03 +0100

    R-07 - Layout for active and inactive sidebars.

--- wp-content/themes/radionica/index.php
+++ wp-content/themes/radionica/index.php
@@ -9,13 +9,15 @@
  */
 get_header();
 
-	if ( have_posts() ) :
-		while ( have_posts() ) : the_post();
+	if ( have_posts() ) : ?>
+		<div class="content-area">
+			<?php while ( have_posts() ) : the_post();
 
-			get_template_part( '/template-parts/entry' );
+				get_template_part( '/template-parts/entry' );
 
-		endwhile; // have_posts()
-	endif; // have_posts()
+			endwhile; // have_posts() ?>
+		</div>
+	<?php endif; // have_posts()
 
 get_sidebar();
 
--- wp-content/themes/radionica/sidebar.php
+++ wp-content/themes/radionica/sidebar.php
@@ -4,8 +4,8 @@
  *
  * @package WordPress
  */
-
-?>
-<aside class="widget-area widget-area-sidebar" role="complementary">
-    <?php dynamic_sidebar( 'sidebar-main' ); ?>
-</aside>
+if ( is_active_sidebar( 'sidebar-main' ) ) : ?>
+	<aside class="widget-area widget-area-sidebar" role="complementary">
+	    <?php dynamic_sidebar( 'sidebar-main' ); ?>
+	</aside>
+<?php endif;
