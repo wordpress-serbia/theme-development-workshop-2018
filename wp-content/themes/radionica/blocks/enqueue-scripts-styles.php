95aa3845318893365e9d52bbd2e1b2c65286f337
Milana Cap <milanazzap@gmail.com>
2019-02-13 19:54:34 +0100

    R-10 - Namespacing? Welcome to PHP 5.3!

--- wp-content/themes/radionica/blocks/enqueue-scripts-styles.php
+++ wp-content/themes/radionica/blocks/enqueue-scripts-styles.php
@@ -5,36 +5,47 @@
  * @package WordPress
  */
 
+// PSR-0 for now
+namespace Radionica\Assets;
+
 /**
  * Enqueue scripts and styles for both, editor and frontend.
  */
-function radionica_enqueue_block_assets() {
+function enqueue_block_assets() {
 
 	wp_enqueue_style(
 		'radionica-blocks-css',
-		get_theme_file_uri( '/blocks/css/blocks.css' )
+		get_theme_file_uri( '/blocks/css/blocks.css' ),
+		[],
+		time()
 	);
 
 	wp_enqueue_script(
 		'radionica-blocks-js',
-		get_theme_file_uri( '/blocks/js/blocks.js' )
+		get_theme_file_uri( '/blocks/js/blocks.js' ),
+		[],
+		time()
 	);
 }
-add_action( 'enqueue_block_assets', 'radionica_enqueue_block_assets' );
+add_action( 'enqueue_block_assets', __NAMESPACE__ . '\enqueue_block_assets' );
 
 /**
  * Enqueue scripts and styles only for editor.
  */
-function radionica_enqueue_block_editor_assets() {
+function enqueue_block_editor_assets() {
 
 	wp_enqueue_style(
 		'radionica-editor-css',
-		get_theme_file_uri( '/blocks/css/editor.css' )
+		get_theme_file_uri( '/blocks/css/editor.css' ),
+		[],
+		time()
 	);
 
 	wp_enqueue_script(
 		'radionica-editor-js',
-		get_theme_file_uri( '/blocks/js/editor.js' )
+		get_theme_file_uri( '/blocks/js/editor.js' ),
+		[],
+		time()
 	);
 }
-add_action( 'enqueue_block_editor_assets', 'radionica_enqueue_block_editor_assets' );
+add_action( 'enqueue_block_editor_assets', __NAMESPACE__ . '\enqueue_block_editor_assets' );
