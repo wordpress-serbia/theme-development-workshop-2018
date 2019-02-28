e3325f0eae2302158fd2e3af7f627361ee54122b
Milana Cap <milanazzap@gmail.com>
2019-02-28 23:11:02 +0100

    R-11 - blocks.registerBlockType - Add system block classes to heading, list and paragraph blocks.

--- wp-content/themes/radionica/blocks/js/editor.js
+++ wp-content/themes/radionica/blocks/js/editor.js
@@ -16,3 +16,36 @@ wp.blocks.registerBlockStyle( 'core/paragraph', {
     name: 'lead-paragraph',
     label: __( 'Lead Paragraph', 'radionica' )
 } );
+
+/**
+ * Adds system block class to list, paragraph, and heading blocks.
+ *
+ * This will add:
+ * .wp-block-heading   - to heading
+ * .wp-block-paragraph - to paragraph
+ * .wp-block-list      - to list
+ *
+ * @link https://wordpress.org/gutenberg/handbook/designers-developers/developers/filters/block-filters/#blocks-registerblocktype
+ *
+ * @param {type}   settings Settings for the block.
+ * @param {string} name     Block name.
+ *
+ * @return {string} Classname.
+ */
+function addBlockClassNames( settings, name ) {
+	if ( 'core/list' !== name && 'core/paragraph' !== name && 'core/heading' !== name ) {
+		return settings;
+	}
+
+	return lodash.assign( {}, settings, {
+		supports: lodash.assign( {}, settings.supports, {
+			className: true,
+		} ),
+	} );
+}
+
+wp.hooks.addFilter(
+	'blocks.registerBlockType',
+	'radionica/core-class-names',
+	addBlockClassNames
+);
