186e3981bea5d3b8a344a1c994604cd564dadc85
Milana Cap <milanazzap@gmail.com>
2019-02-28 22:41:58 +0100

    R-11 - Paragraph block style variation.

--- wp-content/themes/radionica/blocks/js/editor.js
+++ wp-content/themes/radionica/blocks/js/editor.js
@@ -4,4 +4,14 @@
  * Used inside editor.
  */
 
-// alert( 'editor.js is loaded' );
+/**
+ * Add paragraph style variant.
+ *
+ * Adds class 'is-style-lead-paragraph' to Lead paragraph.
+ *
+ * @link https://wordpress.org/gutenberg/handbook/designers-developers/developers/filters/block-filters/#block-style-variations
+ */
+wp.blocks.registerBlockStyle( 'core/paragraph', {
+    name: 'lead-paragraph',
+    label: 'Lead Paragraph'
+} );
