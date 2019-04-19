db50aeb95639ae31cf7eba5dea4b7a282d3b2148
Milana Cap <milanazzap@gmail.com>
2019-04-19 18:59:14 +0200

    R-14 - ACF Flexible content - render layouts.

--- wp-content/themes/radionica/template-acf-flexible.php
+++ wp-content/themes/radionica/template-acf-flexible.php
@@ -22,15 +22,52 @@ get_header();
 
 							if ( get_row_layout() == 'heading_h2' ) :
 
+								if ( get_sub_field( 'heading' ) ) :
+									echo '<h2>' . esc_html( get_sub_field( 'heading' ) ) . '</h2>';
+								endif;
+
 							elseif ( get_row_layout() == 'heading_h3' ) :
+
+								if ( get_sub_field( 'heading' ) ) :
+									echo '<h3>' . esc_html( get_sub_field( 'heading' ) ) . '</h3>';
+								endif;
 
 							elseif ( get_row_layout() == 'heading_h4' ) :
 
+								if ( get_sub_field( 'heading' ) ) :
+									echo '<h4>' . esc_html( get_sub_field( 'heading' ) ) . '</h4>';
+								endif;
+
 							elseif ( get_row_layout() == 'lead_paragraph' ) :
+
+								if ( get_sub_field( 'textarea' ) ) :
+									echo '<p class="lead-paragraph">' . esc_html( get_sub_field( 'textarea' ) ) . '</p>';
+								endif;
 
 							elseif ( get_row_layout() == 'image' ) :
 
+								if ( get_sub_field( 'image' ) ) :
+									$image = get_sub_field( 'image' );
+									echo '<img src="' . esc_url( $image['url'] ) . '" alt="' . esc_attr( $image['alt'] ) . '">';
+								endif;
+
 							elseif ( get_row_layout() == 'wysiwyg' ) :
+
+								if ( get_sub_field( 'editor' ) ) :
+									/**
+									 * Ternary operator
+									 *
+									 * @link https://davidwalsh.name/php-ternary-examples
+									 * @link https://davidwalsh.name/php-shorthand-if-else-ternary-operators
+									 *
+									 * PHP 7+
+									 * @link https://php.net/manual/en/migration70.new-features.php#migration70.new-features.null-coalesce-op
+									 */
+									$background_color = ( get_sub_field( 'background_color' ) ) ? get_sub_field( 'background_color' ) : '#fff';
+
+									echo '<div style="background-color:' . esc_attr( $background_color ) . ';">' . wp_kses_post( get_sub_field( 'editor' ) ) . '</div>';
+
+								endif; // get_sub_field( 'editor' )
 
 							endif; // get_row_layout()
 
